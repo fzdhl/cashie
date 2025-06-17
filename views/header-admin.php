7217f7ba2afd82b72947d63f9725c7efa4965f48
JeudiAugusto <jeudiaugustoasadullah@gmail.com>
2025-06-17 13:14:57 +0700

    menambahkan navbar admin

---
+++ views/header-admin.php
@@ -0,0 +1,42 @@
+<header class="bg-success text-white shadow sticky-top">
+  <nav class="navbar navbar-expand-xl navbar-dark container py-3 px-3 px-md-0">
+    <a class="navbar-brand fw-bold" href="#">💰 Cashie Dashboard</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link text-white" href="?c=adminDashboardController&m=index">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="?c=adminKategoriController&m=index">Kategori</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="?c=adminTanggunganController&m=index">Tanggungan</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="?c=adminDashboardController&m=report">Laporan Keuangan</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="?c=adminCalendarController&m=index">Kalender</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="?c=adminTargetController&m=index">Target</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="?c=amdinArsipController&m=index">Arsip</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Halo, <?= htmlspecialchars($_SESSION['user']->username) ?>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+            <li><a class="dropdown-item" href="?c=ProfileController&m=index">Profile</a></li>
+            <li><a class="dropdown-item" href="?c=DashboardController&m=logout">Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
