3087171a7fa281e7b7dc105791716e21376869b0
alfiahnurfa <alfiahnurfadillahamalia@gmail.com>
2025-06-18 23:00:36 +0700

    fix navbar klik

--- views/header.php
+++ views/header.php
@@ -6,36 +6,69 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a class="nav-link text-white" href="?c=DashboardController&m=index">Dashboard</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="?c=CalendarController&m=index">Transaksi</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="?c=KategoriController&m=index">Kategori</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="?c=TanggunganController&m=index">Tanggungan</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="?c=TargetController&m=index">Target</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="?c=LaporanController&m=report">Laporan Keuangan</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="?c=ArsipController&m=index">Arsip</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Halo, <?= htmlspecialchars($_SESSION['user']->username) ?>
-          </a>
-          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
-            <li><a class="dropdown-item" href="?c=ProfileController&m=index">Profile</a></li>
-            <li><a class="dropdown-item" href="?c=DashboardController&m=logout">Logout</a></li>
-          </ul>
-        </li>
+        <?php if ($_SESSION['user']->privilege == 'admin'):?>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="?c=AdminController&m=index">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="?c=AdminKategoriController&m=index">Kategori</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="?c=TanggunganController&m=index">Tanggungan</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="?c=LaporanController&m=report">Laporan Keuangan</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="?c=adminCalendarController&m=index">Kalender</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="?c=adminTargetController&m=index">Target</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="?c=AdminArsipController&m=index">Arsip</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Halo, <?= htmlspecialchars($_SESSION['user']->username) ?>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+              <li><a class="dropdown-item" href="?c=ProfileController&m=index">Profile</a></li>
+              <li><a class="dropdown-item" href="?c=DashboardController&m=logout">Logout</a></li>
+            </ul>
+          </li>
+        <?php else: ?>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="?c=DashboardController&m=index">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="?c=CalendarController&m=index">Transaksi</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="?c=KategoriController&m=index">Kategori</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="?c=TanggunganController&m=index">Tanggungan</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="?c=TargetController&m=index">Target</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="?c=LaporanController&m=report">Laporan Keuangan</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="?c=ArsipController&m=index">Arsip</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Halo, <?= htmlspecialchars($_SESSION['user']->username) ?>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+              <li><a class="dropdown-item" href="?c=ProfileController&m=index">Profile</a></li>
+              <li><a class="dropdown-item" href="?c=DashboardController&m=logout">Logout</a></li>
+            </ul>
+          </li>
+        <?php endif ?>
       </ul>
     </div>
   </nav>
