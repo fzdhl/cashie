f4cb155ac4f199f770d3da57184cfe54186d769d
Darryl <hippodaoa44@students.unnes.ac.id>
2025-06-19 10:15:27 +0700

    turunin css

--- views/calendar.php
+++ views/calendar.php
@@ -18,12 +18,12 @@
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
     />
-    <link rel="stylesheet" href="./views/styles/styleCalendar.css" />
     <title>Calendar</title>
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
       rel="stylesheet"
     />
+    <link rel="stylesheet" href="./views/styles/styleCalendar.css" />
   </head>
   <body>
     <?php include_once "header.php" ?>
