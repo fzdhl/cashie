0d782168f8e6d5627201dd0aca99620c02439aa5
Darryl <hippodaoa44@students.unnes.ac.id>
2025-06-19 11:55:17 +0700

    duplikat bootstrap link

--- views/adminArsip.php
+++ views/adminArsip.php
@@ -60,7 +60,6 @@
 
   <?php include_once "footer.php" ?>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
   <script>
     // JavaScript for AJAX update of description
     document.querySelectorAll('.deskripsi-form').forEach(form => {
@@ -91,6 +90,5 @@
     });
   </script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-  
 </body>
 </html>
